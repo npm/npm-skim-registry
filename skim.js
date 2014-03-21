e5bd36f7509571693f8118374792293527acc30c
C J Silverio <ceej@npmjs.com>
2014-03-21 13:29:33 -0700

    De-mcouch-ify Skim. All tests pass.

--- skim.js
+++ skim.js
@@ -6,26 +6,75 @@
 //   - if this.skim === this.db, then bump the rev
 //   - otherwise, treat it like a replication, and ?new_edits=false
 
-var MantaCouch = require('mcouch')
-var util = require('util')
-var hh = require('http-https')
-var parse = require('parse-json-response')
-var url = require('url')
-var path = require('path')
-var readmeTrim = require('npm-registry-readme-trim')
+var assert      = require('assert');
+var crypto      = require('crypto');
+var cuttlefish  = require('cuttlefish');
+var EE          = require('events').EventEmitter;
+var follow      = require('follow');
+var hh          = require('http-https')
+var parse       = require('parse-json-response')
+var PassThrough = require('stream').PassThrough;
+var path        = require('path')
+var readmeTrim  = require('npm-registry-readme-trim')
+var url         = require('url')
+var util        = require('util')
 
 module.exports = Skim
-
-util.inherits(Skim, MantaCouch)
 
 function Skim(opts) {
   if (!(this instanceof Skim))
     return new Skim(opts)
 
+  EE.call(this);
+
+  if (!opts || typeof opts !== 'object')
+    throw new TypeError('opts object required');
+
   if (opts.fat && !opts.db)
     opts.db = opts.fat
 
-  MantaCouch.call(this, opts)
+  this.opts = opts;
+
+
+  if (!opts.client || opts.client.constructor.name !== 'MantaClient')
+    throw new TypeError('opts.client of type MantaClient is required');
+  this.client = opts.client;
+
+  if (opts.seqFile && typeof opts.seqFile !== 'string')
+    throw new TypeError('opts.seqFile must be of type string');
+  this.seqFile = opts.seqFile || null;
+  if (this.seqFile)
+    this.seqFile = path.resolve(this.seqFile);
+
+  if (!opts.path || typeof opts.path !== 'string')
+    throw new TypeError('opts.path is required');
+  this.path = opts.path
+    .replace(/\/+$/, '')
+    .replace(/^~~/, '/' + this.client.user);
+
+  if (!opts.db || !url.parse(opts.db).protocol)
+    throw new TypeError('opts.db url is required');
+  this.db = opts.db.replace(/\/+$/, '');
+
+  this.http = url.parse(this.db).protocol === 'https:' ?
+    require('https') : require('http');
+
+  if (opts.inactivity_ms && typeof opts.inactivity_ms !== 'number')
+    throw new TypeError('opts.inactivity_ms must be of type number');
+  this.inactivity_ms = opts.inactivity_ms;
+
+  if (opts.seq && typeof opts.seq !== 'number')
+    throw new TypeError('opts.seq must be of type number');
+  this.seq = opts.seq || 0;
+
+  if (opts.concurrency && typeof opts.concurrency !== 'number')
+    throw new TypeError('opts.concurrency must be of type number');
+  this.concurrency = opts.concurrency;
+
+  this.delete = !!opts.delete;
+
+  this.following = false;
+  this.savingSeq = false;
 
   this.skim = url.parse(opts.skim || opts.db).href
   this.skim = this.skim.replace(/\/+$/, '')
@@ -38,16 +87,141 @@ function Skim(opts) {
     this.registry = this.registry.replace(/\/+$/, '')
   }
 
-
   this.on('put', this.onput)
+
+  this.start();
+}
+util.inherits(Skim, EE);
+
+Skim.prototype.start = function() {
+  if (this.following)
+    throw new Error('Cannot read sequence after follow starts');
+  if (!this.seqFile) {
+    this.seq = 0;
+    this.onReadSeq();
+  } else
+    fs.readFile(this.seqFile, 'ascii', this.onReadSeq.bind(this));
+}
+
+Skim.prototype.onReadSeq = function(er, data) {
+  if (er && er.code === 'ENOENT')
+    data = 0;
+  else if (er)
+    return this.emit('error', er);
+
+  if (data === undefined)
+    data = null
+  if (!+data && +data !== 0)
+    return this.emit('error', new Error('invalid data in seqFile'));
+
+  data = +data;
+  this.seq = +data;
+  this.follow = follow({
+    db: this.db,
+    since: this.seq,
+    inactivity_ms: this.inactivity_ms
+  }, this.onChange.bind(this));
+  this.following = true;
+}
+
+Skim.prototype.saveSeq = function(file) {
+  file = file || this.seqFile;
+  if (!file && !this.seqFile)
+    return
+  if (!file)
+    throw new Error('invalid sequence file: ' + file);
+  if (this.savingSeq)
+    return
+
+  this.savingSeq = true;
+  var t = file + '.TMP'
+  var data = this.seq + '\n'
+  fs.writeFile(t, data, 'ascii', function(er) {
+    if (er)
+      return this.afterSave(er)
+    fs.rename(t, file, this.afterSave.bind(this))
+  }.bind(this))
+}
+
+Skim.prototype.afterSave = function(er) {
+  if (er)
+    this.emit('error', er);
+  this.savingSeq = false;
+}
+
+Skim.prototype.onChange = function(er, change) {
+  if (er)
+    return this.emit('error', er);
+
+  this.seq = change.seq;
+
+  // Please don't delete the entire store in Manta, kthx
+  if (!change.id)
+    return;
+
+  if (change.deleted)
+    this.rm(change);
+  else
+    this.put(change);
 }
 
 Skim.prototype.put = function(change) {
   if (change.id.match(/^_design\//) && this.db !== this.skim) {
-    this.putDesign(change)
-  } else {
-    return MantaCouch.prototype.put.apply(this, arguments)
+    return this.putDesign(change)
   }
+
+  if (change.id !== encodeURIComponent(change.id)) {
+    console.error('WARNING: Skipping %j\nWARNING: See %s', change.id,
+                 'https://github.com/joyent/node-manta/issues/157')
+    return
+  }
+
+  this.pause();
+  var query = 'att_encoding_info=true&revs=true';
+  var u = url.parse(this.db + '/' + change.id + '?' + query);
+  this.http.get(u, parse(function(er, doc, res) {
+    if (er)
+      return this.emit('error', er);
+    change.doc = doc;
+    this._put(change);
+  }.bind(this)))
+}
+
+Skim.prototype._put = function(change) {
+  this.emit('put', change);
+  var doc = change.doc;
+
+  var files = Object.keys(doc._attachments || {}).reduce(function (s, k) {
+    var att = doc._attachments[k];
+    // Gzip-encoded attachments are lying liars playing lyres
+    if (att.encoding === 'gzip') {
+      delete att.digest;
+      delete att.length;
+    }
+    s['_attachments/' + k] = doc._attachments[k];
+    return s;
+  }, {});
+
+  var json = new Buffer(JSON.stringify(doc) + '\n', 'utf8');
+  files['doc.json'] = {
+    type: 'application/json',
+    name: 'doc.json',
+    length: json.length
+  };
+
+  cuttlefish({
+    path: this.path + '/' + doc._id,
+    client: this.client,
+    files: files,
+    getMd5: this.getMd5.bind(this, change, json),
+    request: this.getFile.bind(this, change, json),
+    delete: this.delete,
+    concurrency: this.concurrency
+  })
+    .on('send', this.emit.bind(this, 'send', change))
+    .on('delete', this.emit.bind(this, 'delete', change))
+    .on('error', this.emit.bind(this, 'error'))
+    .on('complete', this.onCuttleComplete.bind(this, change));
 }
 
 Skim.prototype.putDesign = function(change) {
@@ -65,6 +239,14 @@ Skim.prototype.putDesign = function(change) {
   }.bind(this)))
 }
 
+Skim.prototype.rm = function(change) {
+  if (this.delete) {
+    this.emit('rm', change);
+    this.pause();
+    this.client.rmr(this.path + '/' + change.id, this.onRm.bind(this, change));
+  }
+}
+
 Skim.prototype.onRm = function(change, er) {
   // If there's an error, or invalid change, just let mcouch handle it
   // If the db isn't the same as the skim, then presumably it's already
@@ -80,7 +262,7 @@ Skim.prototype.onRm = function(change, er) {
   hh.request(h, function(res) {
     // if already gone, then great
     if (res.statusCode === 404)
-      return MantaCouch.prototype.onRm.call(this, change, er)
+      return this._onRm(change, er)
 
     var rev = res.headers.etag.replace(/^"|"$/g, '')
     var d = url.parse(this.skim + '/' + change.id + '?rev=' + rev)
@@ -88,13 +270,22 @@ Skim.prototype.onRm = function(change, er) {
     hh.request(d, parse(function(er, data, res) {
       // If we got an error (incl 404) let mcouch handle it
       // otherwise, delete again, until all conflict revs are gone.
-      if (er)
-        MantaCouch.prototype.onRm.call(this, change, er)
+      if (er) {
+        this._onRm(change, er)
+      }
       else
         this.onRm(change, er)
     }.bind(this))).end()
   }.bind(this)).end()
 }
+
+Skim.prototype._onRm = function(change, er) {
+  if (!er || er.statusCode === 404)
+    this.resume();
+  else
+    this.emit('error', er);
+}
+
 
 Skim.prototype.onput = function(change) {
   var doc = change.doc
@@ -156,13 +347,28 @@ Skim.prototype.onCuttleComplete = function(change, results) {
 
   if (!k.length && !extraReadmes.length && this.skim === this.db) {
     // no disallowed attachments, just leave as-is
-    return MantaCouch.prototype.onCuttleComplete.call(this, change, results)
+    return this._onCuttleComplete(change, results)
   }
 
   // It's easier if we always have an _attachments, even if empty
   doc._attachments = {}
 
   this.putBack(change, results)
+}
+
+
+Skim.prototype._onCuttleComplete = function(change, results) {
+  this.emit('complete', change, results);
+  this.resume();
+};
+
+Skim.prototype.stop =
+Skim.prototype.close =
+Skim.prototype.destroy = function() {
+  if (this.client)
+    this.client.close();
+  if (this.follow)
+    this.follow.stop();
 }
 
 Skim.prototype.putBack = function(change, results) {
@@ -202,6 +408,46 @@ Skim.prototype.putBack = function(change, results) {
     if (er)
       this.emit('error', er)
     else
-      MantaCouch.prototype.onCuttleComplete.call(this, change, results)
+      this._onCuttleComplete(change, results)
   }.bind(this))).end(body)
 }
+
+Skim.prototype.getMd5 = function(change, json, file, cb) {
+  if (file.name === 'doc.json')
+    var md5 = crypto.createHash('md5').update(json).digest('base64');
+  cb(null, md5);
+}
+
+Skim.prototype.getFile = function(change, json, file, cb) {
+  if (file.name === 'doc.json')
+    this.streamDoc(json, file, cb)
+  else
+    this.getAttachment(change, file, cb)
+}
+
+Skim.prototype.streamDoc = function(json, file, cb) {
+  var s = new PassThrough();
+  s.end(json);
+  cb(null, s);
+}
+
+Skim.prototype.getAttachment = function(change, file, cb) {
+  var a = path.dirname(file.name).replace(/^_attachments/, change.id);
+  var f = encodeURIComponent(path.basename(file.name))
+  a += '/' + f
+  this.emit('attachment', change, file);
+  var u = this.db + '/' + a;
+  this.http.get(u, function(res) {
+    cb(null, res);
+  }).on('error', cb);
+}
+
+Skim.prototype.pause = function() {
+  this.follow.pause();
+};
+
+Skim.prototype.resume = function() {
+  this.saveSeq();
+  this.follow.resume();
+}
+
