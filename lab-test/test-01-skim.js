5914fd727d373612b649e347b8dcee7c4b2857b9
C J Silverio <ceej@npmjs.com>
2014-04-11 10:15:45 -0700

    Ported the meaty sync test.

--- lab-test/test-01-skim.js
+++ lab-test/test-01-skim.js
@@ -24,7 +24,60 @@ describe('skimming', function()
         done();
     });
 
-    it('emits expected events when operating');
+    it('emits expected events on a first sync', function(done)
+    {
+        var skimmer;
+        var expected =
+        {
+            'put test-package' : 1,
+            'attachment test-package/_attachments/test-package-0.0.0.tgz' : 1,
+            'sent test-package/doc.json' : 1,
+            'sent test-package/_attachments/test-package-0.0.0.tgz' : 1,
+            'complete test-package': 1
+        };
+
+        function checkEvent()
+        {
+            var str = util.format.apply(util, arguments);
+            expected.must.have.property(str);
+
+            expected[str]--;
+            if (expected[str] === 0)
+                delete expected[str];
+
+            if (Object.keys(expected).length === 0)
+            {
+                skimmer.destroy();
+                done();
+            }
+        }
+
+        var opts =
+        {
+            debug         : true,
+            client        : client,
+            db            : 'http://localhost:15984/registry',
+            path          : '.',
+            seq           : 0,
+            inactivity_ms : 10000
+        };
+
+        skimmer = skim(opts)
+        .on('put', function(change) { checkEvent('put %s', change.id); })
+        .on('rm', function(change) { checkEvent('rm %s', change.id); })
+        .on('send', function(change, file) { checkEvent('sent %s', file); })
+        .on('delete', function(change, file) { checkEvent('delete %s', file); })
+        .on('attachment', function(change, file) { checkEvent('attachment %s', file); })
+        .on('complete', function(change, results) { checkEvent('complete %s', change.id); })
+        .on('error', function(err)
+        {
+            if (err.stackTrace)
+                console.error(err.stackTrace());
+            else
+                console.error(err);
+            throw(err);
+        });
+    });
 
     it('writes files with correct md5 sums', function(done)
     {
