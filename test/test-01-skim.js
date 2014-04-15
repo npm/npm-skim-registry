66f4b766d78d8de90e8e293ef6dace7a33e62a01
C J Silverio <ceej@npmjs.com>
2014-04-14 17:31:07 -0700

    Excellent: a second publication test (at a live skimmer this time) reveals a loop in document puts. If we don't skim, we ought to do NOTHING. Fix incoming.

--- test/test-01-skim.js
+++ test/test-01-skim.js
@@ -7,8 +7,10 @@ var
     before   = lab.before,
     after    = lab.after,
     demand   = require('must'),
+    fs       = require('fs'),
     http     = require('http'),
     parse    = require('parse-json-response'),
+    path     = require('path'),
     skim     = require('../skim.js'),
     url      = require('url'),
     util     = require('util')
@@ -70,11 +72,11 @@ describe('skimming', function()
         done();
     });
 
+    var skimmer;
 
     it('emits expected events on a first sync', { timeout: 20000 }, function(done)
     {
         var client = createTestClient();
-        var skimmer;
         var expected =
         {
             'put test-package' : 1,
@@ -95,7 +97,7 @@ describe('skimming', function()
 
             if (Object.keys(expected).length === 0)
             {
-                skimmer.destroy();
+                skimmer.removeAllListeners();
                 done();
             }
         }
@@ -107,11 +109,12 @@ describe('skimming', function()
             db            : 'http://localhost:15984/registry',
             path          : '.',
             seq           : 0,
+            seqFile       : './test/couch-tmp/sequence',
             inactivity_ms : 10000
         };
 
         skimmer = skim(opts)
-        .on('put', function(change) { checkEvent('put %s', change.id); })
+        .once('put', function(change) { checkEvent('put %s', change.id); })
         .on('rm', function(change) { checkEvent('rm %s', change.id); })
         .on('send', function(change, file) { checkEvent('sent %s', file); })
         .on('delete', function(change, file) { checkEvent('delete %s', file); })
@@ -155,6 +158,101 @@ describe('skimming', function()
         }));
     });
 
-    it('removes attachments on unpublish (if delete is set)');
-    it('does not remove attachments if delete is not set');
+
+    function publishPackage(callback)
+    {
+        var testPkg = require('./fixtures/semver.json');
+        var tf = path.resolve(__dirname, 'fixtures/test-package-0.0.0.tgz');
+        var tgzData = fs.readFileSync(tf, 'base64');
+        testPkg._attachments['semver-0.1.0.tgz'].data = tgzData;
+        testPkg._attachments['semver-0.1.0.tgz'].stub = false;
+
+        var body = new Buffer(JSON.stringify(testPkg));
+
+        var u = url.parse('http://admin:admin@localhost:15984/registry/semver');
+        u.method = 'PUT';
+        u.headers =
+        {
+            'content-type': 'application/json',
+            'content-length': body.length,
+            connection: 'close'
+        };
+
+        http.request(u, function(response)
+        {
+            response.statusCode.must.equal(201);
+            if (response.statusCode !== 201)
+                response.pipe(process.stderr)
+            callback();
+        })
+        .on('error', function(err)
+        {
+            demand(err).be.falsy();
+        })
+        .end(body);
+    }
+
+    it('say hello to a second publish', { timeout: 20000 }, function(done)
+    {
+        var client = createTestClient();
+        var expected2 =
+        {
+'put test-package' : 1,
+'attachment test-package/_attachments/test-package-0.0.0.tgz' : 1,
+'sent test-package/doc.json' : 1,
+'sent test-package/_attachments/test-package-0.0.0.tgz' : 1,
+'complete test-package': 1,
+            'put semver' : 3,
+            'attachment semver/_attachments/semver-0.1.0.tgz' : 1,
+            'sent semver/doc.json' : 3,
+            'sent semver/_attachments/semver-0.1.0.tgz' : 1,
+            'complete semver': 3
+        };
+
+        function checkEvent()
+        {
+            var str = util.format.apply(util, arguments);
+            console.log(str);
+            expected2.must.have.property(str);
+
+            expected2[str]--;
+            if (expected2[str] === 0)
+                delete expected2[str];
+
+            if (Object.keys(expected2).length === 0)
+            {
+                console.log('we are done baby')
+                skimmer.removeAllListeners();
+                skimmer.destroy();
+                done();
+            }
+        }
+
+        skimmer
+        .on('put', function(change) { checkEvent('put %s', change.id); })
+        .on('rm', function(change) { checkEvent('rm %s', change.id); })
+        .on('send', function(change, file) { checkEvent('sent %s', file); })
+        .on('delete', function(change, file) { checkEvent('delete %s', file); })
+        .on('attachment', function(change, file) { checkEvent('attachment %s', file); })
+        .on('complete', function(change, results) { checkEvent('complete %s', change.id); })
+        .on('error', function(err)
+        {
+            console.log('woah skimmer threw an error!');
+            if (err.stackTrace)
+                console.error(err.stackTrace());
+            else
+                console.error(err);
+            demand(err).be.falsy();
+        });
+
+        publishPackage(function()
+        {
+            console.log('published semver');
+        });
+    });
+
+    it('removes attachments on unpublish (if delete is set)', function(done)
+    {
+        done();
+    });
 });
