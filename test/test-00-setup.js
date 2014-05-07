96609eddcde4cd9f4b8be565e5425dc62a86b6ed
C J Silverio <ceej@npmjs.com>
2014-05-06 17:30:01 -0700

    MULTISKIM all squashed and cleaned up.
    * Replaced cuttlefish with multi-fs.
    * Removed the dependency on mcouch; restructured signficantly.
    * Added bunyan logging output for more diagnosible logs.
    * Added 'log' events to emit debugging information.
    * Switched from tap testing to lab testing.
    * Expanded the tests somewhat & based them on a couchdb snapshot starting point.
    * Tweaked command-line options slightly.
    * Added semicolons like a stodgy person.
    * Added a functional test in `npm run dev` that skims from fullfatdb into
      a local couchdb.

---
+++ test/test-00-setup.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var
+    lab      = require('lab'),
+    describe = lab.describe,
+    it       = lab.it,
+    before   = lab.before,
+    demand   = require('must'),
+    fs       = require('fs'),
+    Manta    = require('manta-client'),
+    mkdirp   = require('mkdirp'),
+    path     = require('path'),
+    spawn    = require('child_process').spawn
+    ;
+
+describe('setup', function()
+{
+    it('can create test destination directories', function(done)
+    {
+        var fix = path.join(__dirname, 'tmp', 'registry-testing');
+        mkdirp.sync(path.join(fix, '0'));
+        mkdirp.sync(path.join(fix, '1'));
+        done();
+    });
+
+    it('can create a manta destination directory', { timeout: 20000 }, function(done)
+    {
+        var manta = Manta(process.argv, process.env);
+        manta.mkdirp('~~/stor/registry-testing/', function(err)
+        {
+            demand(err).be.falsy();
+            manta.close();
+            done();
+        });
+    });
+
+    // run with the cwd of the main program.
+    var cwd = path.dirname(__dirname);
+
+    var conf = path.resolve(__dirname, 'fixtures', 'couch.ini');
+    var pidfile = path.resolve(__dirname, 'couch-tmp', 'pid');
+    var logfile = path.resolve(__dirname, 'couch-tmp', 'couch.log');
+    var started = /Apache CouchDB has started on http:\/\/127\.0\.0\.1:15984\/\n$/;
+
+    it('can set up couch tmp directory', function(done)
+    {
+        fs.mkdirSync(path.join(__dirname, 'couch-tmp'));
+
+        var files = ['_replicator.couch', '_users.couch', 'registry.couch'];
+        files.forEach(function(f)
+        {
+            var src = path.resolve(__dirname, 'fixtures', f);
+            var dest = path.resolve(__dirname, 'couch-tmp', f);
+            fs.writeFileSync(dest, fs.readFileSync(src))
+        });
+
+        done();
+    });
+
+    it('can start couch as a zombie child', { timeout: 15000 }, function(done)
+    {
+        var fd = fs.openSync(pidfile, 'wx');
+        try { fs.unlinkSync(logfile); } catch (er) {}
+        var child = spawn('couchdb', ['-a', conf], {
+            detached: true,
+            stdio: 'ignore',
+            cwd: cwd
+        });
+        child.unref();
+
+        child.pid.must.be.truthy();
+        fs.writeSync(fd, child.pid + '\n');
+        fs.closeSync(fd);
+
+        // wait for it to create a log, give it 5 seconds
+        var start = Date.now();
+
+        fs.readFile(logfile, function R (err, log)
+        {
+            log = log ? log.toString() : '';
+            if (!err && !log.match(started))
+                err = new Error('not started yet');
+            if (err)
+            {
+                if (Date.now() - start < 5000)
+                    return setTimeout(function () { fs.readFile(logfile, R) }, 1000);
+                else
+                    demand(err).be.falsy();
+            }
+            done();
+        });
+    });
+
+});
