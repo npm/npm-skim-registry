59f7b5992503074d2b8a9dedf2e2e51587ca22cf
C J Silverio <ceej@npmjs.com>
2014-10-19 19:47:53 -0700

    Bumped multi-fs version.
    Bumped other dep versions while I was at it.
    Removed manta from the dev test targets.

--- test/dev-targets.js
+++ test/dev-targets.js
@@ -2,12 +2,6 @@ var
     mkdirp = require('mkdirp'),
     path = require('path');
 
-if (!process.env.MANTA_KEY_ID || !process.env.MANTA_USER || !process.env.MANTA_URL)
-{
-    console.error('not ok - need manta environs');
-    process.exit(1);
-}
-
 if (!process.env.SSH_AUTH_SOCK)
 {
     console.error('not ok - only ssh-agent authentication is supported');
@@ -21,5 +15,4 @@ module.exports =
 [
     { type: 'fs', path: devbase + '/0' },
     'ssh://localhost:' + devbase + '/1',
-    '~~/stor/skimdev/2',
 ];
